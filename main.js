18d30c98c58fba543ac419a031dc19f80681351d
+ <+>
2023-04-04 10:36:42 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,52 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeGlyphs = document.querySelectorAll(".like-glyph");
+
+likeGlyphs.forEach(likeGlyph => {
+  likeGlyph.addEventListener("click", handleLikeClick);
+});
+
+
+function handleLikeClick(event){
+  if (event.target.innerHTML === EMPTY_HEART){
+    mimicServerCall().then(() => {
+      event.target.classList.add("activated-heart");
+      event.target.innerHTML = FULL_HEART;
+
+    }).catch((error) => {
+      let modal = document.querySelector("#modal");
+      modal.classList.remove("hidden");
+      let modalMessage = document.querySelector("#modal-message");
+      modalMessage.textContent = error;
+
+      function removeErrorMessage() {
+        modal.classList.add("hidden");
+      }
+
+      // Call removeErrorMessage() after 3 seconds
+      setTimeout(removeErrorMessage, 3000);
+    })
+  }
+
+  if (event.target.innerHTML === FULL_HEART){
+    mimicServerCall()
+    .then(() => {
+      event.target.classList.remove("activated-heart");
+      event.target.innerHTML = EMPTY_HEART;
+
+    })
+  }
+
+}
+
+if (element.classList.contains('class1')) {
+  console.log('class1 exists');
+}
+if (element.classList.contains('class2')) {
+  console.log('class2 exists');
+}
+
 
 
 
